bf808d8d511497f25fcccb9d660a584899de41e9
LLuizXL <luiz.mwlo76@gmail.com>
2024-09-25 16:50:03 -0300

    Estilização página View de categorias e produtos (Edit, Create e Delete)

--- AppVendas/Models/Produto.cs
+++ AppVendas/Models/Produto.cs
@@ -8,21 +8,34 @@ namespace AppVendas.Models
 
         [Display(Name = "Nome")]
         [Required(ErrorMessage = "Insira o nome do produto!")]
+        [MaxLength(100, ErrorMessage = "O campo deve ter no máximo, 100 caracteres!")]
+        [MinLength(3, ErrorMessage = "O campo deve ter no mínimo, 3 caracteres!")]
         public string ProdutoNome { get; set; }
+
+
 
         [Display(Name = "Preço")]
         [Required(ErrorMessage = "Insira o valor do produto!")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor do produto deve ser um número positivo!")]
         public double Valor { get; set; }
+
+
 
         [Display(Name = "Estoque Disponível")]
         [Required(ErrorMessage = "Insira a quantidade do estoque!")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor do produto deve ser um número positivo!")]
         public double QtdEstoque { get; set; }
 
 
+
+
         [Display(Name = "Ativo?")]
-        public bool CadastroAtivo { get; set; }
+        public bool CadastroAtivo { get; set; } = true;
 
         //Chave Estrangeira
+
+        [Required(ErrorMessage = "Por favor, selecione uma categoria!")]
+        [Display(Name = "Categoria")]
         public Guid CategoriaId { get; set; }
         public Categoria? Categoria { get; set; }
 
